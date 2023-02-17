90eb1909c382df13503f563022f5eb0e1a8b329b
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2023-02-17 13:02:35 +0300

    add necessary files

---
+++ How to Build a Like System?/like.sql
@@ -0,0 +1,16 @@
+--Like System Implementation
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFUALT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFUALT CURRENT_TIMESTAMP,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+    comment_id INTEGER REFERENCES comments(id) ON DELETE CASCADE
+    CHECK(
+        COALESCE((post_id)::BOOLEAN::INTEGER,0)
+        +
+        COALESCE((comment_id)::BOOLEAN::INTEGER,0)
+        = 1
+    ),
+    UNIQUE(user_id,post_id,comment_id)
+);
---
+++ How to Build a Mention System?/README.md
@@ -0,0 +1,5 @@
+# Tables
+
+![Tables](tables.png)
+
+Users should be able to tag another users on a post. So we declared X and Y to get the coordinates on a post.(X,Y)
